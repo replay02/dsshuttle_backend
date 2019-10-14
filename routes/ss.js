5c841b53bf5ca83e4371aeb6904ff7af1298057d
YoungHyun Kim <replay02@nate.com>
2019-10-14 12:34:44 +0900

    오류수정

--- routes/ss.js
+++ routes/ss.js
@@ -152,16 +152,17 @@ module.exports = function(app,SmtpPool, pushServerKey,ShuttleTimes,SsUser,Boardc
     // 사용자 이메일 인증 후 관리자 승인 완료 처리
     router.post('/api/confirmUser' , function(req,res) {
 
-      SsUser.update({ id: req.body.id }, {register_confirm : req.body.register}, function(err, output){
+      SsUser.update({ id: req.body.id }, {register_confirm : true}, function(err, output){
         //if(err) res.status(500).json({ error: 'database failure' });
         if(err) {
           res.json({ resCode: 500, resMsg:err });
           console.log(err);
+          return;
         }
 
         console.log('confirmUser updated Successfully');
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.json({ resCode: 200, resMsg:'OK' });
+        // res.writeHead(200, {'Content-Type': 'application/json'});
+        res.json({resCode: 200, resMsg:'OK'});
       });
     });
 
