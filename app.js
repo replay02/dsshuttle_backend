025e13d40ac119795819b9cc1e074372e6e4c036
YoungHyun Kim <replay02@nate.com>
2019-11-19 09:14:44 +0900

    test

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 var expressWs = require("express-ws")(app_socket); 
 var webso = require("./routes/ws"); 
 app_socket.use("/websocket",webso); 
-app_socket.listen(80); 
+app_socket.listen(8080); 
 
 
 
